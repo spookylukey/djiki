3dff3995e99e7b09e027b5326dce3864143ad191
Luke Plant <L.Plant.98@cantab.net>
2011-08-22 11:37:10 +0100

    Removed things that are specific to djiki project
    In preparation for making this app more 'app-like', things that
    are specific to djiki.info have been removed

--- settings.py
+++ settings.py
@@ -109,7 +109,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.contrib.messages.context_processors.messages',
-    'ga_processor.google_analytics',
 )
 
 DJIKI_IMAGES_PATH = 'djimages/'        # relative to MEDIA_ROOT
