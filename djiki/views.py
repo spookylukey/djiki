89ab17e73f52a9ed73832ebfc8298621160ce8d2
Luke Plant <L.Plant.98@cantab.net>
2011-08-22 11:03:05 +0100

    Fixed use of various features deprecated Django 1.3
    Including
     - function based generic views
     - Old URL tag

--- djiki/views.py
+++ djiki/views.py
@@ -3,11 +3,10 @@ from django.conf import settings
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden, HttpResponseNotFound
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.template import RequestContext, loader
 from django.utils.translation import ugettext as _
 from django.utils.safestring import mark_safe
-from django.views.generic.simple import direct_to_template
 from urllib import urlencode, quote
 from . import models, forms, utils
 
@@ -50,8 +49,8 @@ def view(request, title, revision_pk=None):
         response['Content-Disposition'] = 'attachment; filename=%s.txt' % quote(title.encode('utf-8'))
         response.write(revision.content)
         return response
-    return direct_to_template(request, 'djiki/view.html',
-            {'page': page, 'revision': revision})
+    return render(request, 'djiki/view.html',
+                  {'page': page, 'revision': revision})
 
 
 def edit(request, title):
@@ -86,8 +85,8 @@ def edit(request, title):
                 form.save()
                 return HttpResponseRedirect(
                         reverse('djiki-page-view', kwargs={'title': url_title}))
-    return direct_to_template(request, 'djiki/edit.html',
-            {'form': form, 'page': page, 'preview_content': preview_content})
+    return render(request, 'djiki/edit.html',
+                  {'form': form, 'page': page, 'preview_content': preview_content})
 
 
 def history(request, title):
@@ -97,7 +96,7 @@ def history(request, title):
     page_title = utils.deurlize_title(title)
     page = get_object_or_404(models.Page, title=page_title)
     history = page.revisions.order_by('-created')
-    return direct_to_template(request, 'djiki/history.html', {'page': page, 'history': history})
+    return render(request, 'djiki/history.html', {'page': page, 'history': history})
 
 
 def diff(request, title):
@@ -113,8 +112,8 @@ def diff(request, title):
         return HttpResponseNotFound()
     dmp = diff_match_patch()
     diff = dmp.diff_compute(from_rev.content, to_rev.content, True, 2)
-    return direct_to_template(request, 'djiki/diff.html',
-            {'page': page, 'from_revision': from_rev, 'to_revision': to_rev, 'diff': diff})
+    return render(request, 'djiki/diff.html',
+                  {'page': page, 'from_revision': from_rev, 'to_revision': to_rev, 'diff': diff})
 
 
 def revert(request, title, revision_pk):
@@ -143,8 +142,8 @@ def revert(request, title, revision_pk):
                     {'time': src_revision.created}
         form = forms.PageEditForm(data=request.POST or None, instance=new_revision, page=page,
                 initial={'content': src_revision.content, 'description': description})
-    return direct_to_template(request, 'djiki/edit.html',
-            {'page': page, 'form': form, 'src_revision': src_revision})
+    return render(request, 'djiki/edit.html',
+                  {'page': page, 'form': form, 'src_revision': src_revision})
 
 
 def undo(request, title, revision_pk):
@@ -194,7 +193,7 @@ def undo(request, title, revision_pk):
                     urlencode(urldata)))
         form = forms.PageEditForm(data=request.POST or None, page=page,
                 initial={'content': content, 'description': description})
-    return direct_to_template(request, 'djiki/edit.html', {'page': page, 'form': form})
+    return render(request, 'djiki/edit.html', {'page': page, 'form': form})
 
 
 def image_new(request):
@@ -206,7 +205,7 @@ def image_new(request):
             form.save()
             return HttpResponseRedirect(
                     reverse('djiki-image-view', kwargs={'name': form.instance.image.name}))
-    return direct_to_template(request, 'djiki/image_edit.html', {'form': form})
+    return render(request, 'djiki/image_edit.html', {'form': form})
 
 
 def image_view(request, name):
@@ -215,7 +214,7 @@ def image_view(request, name):
         return HttpResponseRedirect(reverse('djiki-image-view', kwargs={'name': url_name}))
     image_name = utils.deurlize_title(name)
     image = get_object_or_404(models.Image, name=image_name)
-    return direct_to_template(request, 'djiki/image_view.html', {'image': image})
+    return render(request, 'djiki/image_view.html', {'image': image})
 
 
 def image_edit(request, name):
@@ -235,7 +234,7 @@ def image_edit(request, name):
             form.save()
             return HttpResponseRedirect(
                     reverse('djiki-image-view', kwargs={'name': url_name}))
-    return direct_to_template(request, 'djiki/image_edit.html', {'form': form})
+    return render(request, 'djiki/image_edit.html', {'form': form})
 
 
 def image_history(request, name):
@@ -245,4 +244,4 @@ def image_history(request, name):
     image_name = utils.deurlize_title(name)
     image = get_object_or_404(models.Image, name=image_name)
     history = image.revisions.order_by('-created')
-    return direct_to_template(request, 'djiki/image_history.html', {'image': image, 'history': history})
+    return render(request, 'djiki/image_history.html', {'image': image, 'history': history})
